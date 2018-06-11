a95e4306e5d9b57a9bb0f958b0ec28de5fe011eb
wheretanpopo <36845403+wheretanpopo@users.noreply.github.com>
2018-06-11 20:46:36 +0900

    pwd recheck commit

--- flowerRoadProj_master/src/com/fr/jsp/member/controller/MemberPwdRecheck.java
+++ flowerRoadProj_master/src/com/fr/jsp/member/controller/MemberPwdRecheck.java
@@ -25,15 +25,18 @@ public class MemberPwdRecheck extends HttpServlet {
 		String num = (String)session.getAttribute("memberNum");
 		String pwd = (String)request.getParameter("pwd");
 		int sel = Integer.parseInt(request.getParameter("sel"));
-		
+		System.out.println(pwd);
+		System.out.println("sel"+sel);
 		int result = new MemberService().pwdRecheck(num, pwd);
-		
+		System.out.println("last"+result);
 		if(result != 0){
 			switch(sel){
 			case 1 : response.sendRedirect("views/myPage/myPage_pwdChange.jsp"); break;
 			case 2 : response.sendRedirect("views/myPage/myPage_infoModify.jsp"); break;
 			case 3 : response.sendRedirect("views/myPage/myPage_memberWithdrawal.jsp"); break;
 			} 
+		} else {
+			response.sendRedirect("views/common/errorPage.jsp");
 		}
 		
 		
